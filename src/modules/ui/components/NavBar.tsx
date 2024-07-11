075338887619eda56e21b281039ce954bc3a887e
Alexey Selivanov <lazypacman@gmail.com>
2024-07-11 20:03:08 +0400

    Add favicon

--- src/modules/ui/components/NavBar.tsx
+++ src/modules/ui/components/NavBar.tsx
@@ -15,9 +15,10 @@ export const NavBar = observer(() => {
   if (settings.zen) {
     return null;
   }
+
   return (
     <nav className="sticky top-0 z-50 flex h-8 w-full justify-between border-b border-b-slate-300 bg-slate-100 px-4">
-      <ul className="flex items-center gap-2 p-1 ">
+      <ul className="flex items-center gap-2 p-1">
         {Object.entries(links).map(([to, text]) => (
           <li key={to}>
             <Link className="lexi-button" to={to}>
